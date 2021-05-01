e3ed2922527312ea7095e9199295ab74fa441698
anjali-001 <anjali001.as@gmail.com>
2021-05-01 13:18:16 +0530

    responsiveness and footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import FeatherIcon from 'feather-icons-react';
 import './Footer.css'
 
 function Footer() {
     return (
-        <div>
-            <div style={{height:"1px", width:"60vw", backgroundColor:"#ACB1B1", margin:"auto"}}>
+        <div className="footer">
+
+            {/* Horizontal line */}
+            <div className="footer-line" style={{height:"1px", width:"24vw", backgroundColor:"#ACB1B1", margin:"auto"}}>
 
             </div>
+            <div className="footer-content">
+            <div className="footer-icons">
+                <FeatherIcon icon="github"/>
+                <FeatherIcon icon="twitter"/>
+                <FeatherIcon icon="linkedin"/>
+                <FeatherIcon icon="instagram"/>
+            </div>
+            <div> @ ANJALI</div>
+            </div>
+
         </div>
     )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 import './Navbar.css'
 import FeatherIcon from "feather-icons-react";
 import logodark from '../../assets/logodark.png'
-
+import menuIcon from '../../assets/menu-icon.svg'
 
 
 function Navbar() {
@@ -14,9 +14,10 @@ function Navbar() {
                 <Link to='/'><p>Projects</p></Link>
                 <Link to='/'><p>Blog</p></Link>
                 <Link to='/'><p>Resume</p></Link>
-                <Link><p><FeatherIcon icon="bookmark"/></p></Link>
-                <Link><p><FeatherIcon icon="send"/></p></Link>
-                <Link><p className="btn-theme"><FeatherIcon icon="sun"/></p></Link>
+                <img className="navbar-menu" src={menuIcon}/>
+                <Link className="nav-icon"><p><FeatherIcon icon="bookmark"/></p></Link>
+                <Link className="nav-icon"><p><FeatherIcon icon="send"/></p></Link>
+                <Link className="nav-icon"><p className="btn-theme"><FeatherIcon icon="sun"/></p></Link>
                 </div>
                 
             

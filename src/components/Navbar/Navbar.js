77827d7c4056fd0f6bb0fd061f05b3a956a3c28f
anjali-001 <anjali001.as@gmail.com>
2021-08-16 22:17:38 +0530

    link added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -31,7 +31,7 @@ function Navbar() {
                 <Link to='/blog'>
                     <p>Blog</p>
                 </Link>
-                <a href='https://drive.google.com/file/d/1Hyn_x6lkQwwio0uKxuGYeqDl6YCQ262D/view?usp=sharing'>
+                <a href='https://drive.google.com/file/d/1ARIJYlB3c6DWwIAjfIe-YSS05MOfUWNQ/view?usp=sharing'>
                     <p>Resume</p>
                 </a>
                 

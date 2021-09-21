82e2997855658c3b2b65c25d414abcc7573a0b30
anjali-001 <anjali001.as@gmail.com>
2021-09-21 15:48:02 +0530

    update

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -32,7 +32,7 @@ function Home() {
           functionality at a time.
         </p>
         <h3>Open source</h3>
-        <p>As someone who has contantly learns from open source, it feels good to be able to contribute to projects that is used by someone. My recent contribution,</p>
+        <p>As someone who contantly learns from open source, it feels good to be able to contribute to projects that is used by someone. My recent contribution,</p>
         <p>
           <span className="link-word"><a href="https://github.com/SimplQ/simplQ-frontend" className="github-link">SimpleQ</a></span>: It is a completely web based queue management solution that anyone can use to create instant virtual queues.
         </p>

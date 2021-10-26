7f58ddd3ce7f396f7059b3fc4556527094b979b9
anjali-001 <anjali001.as@gmail.com>
2021-10-26 23:57:14 +0530

    updated home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -11,30 +11,66 @@ function Home() {
         <p>
           Full stack developer and indie-builder who loves to play around and
           tinker with JavaScript. You can find most of my work{" "}
-          <span className="home-word"><a className="github-link" href="https://github.com/anjali-001">here</a></span>
+          <span className="home-word">
+            <a className="github-link" href="https://github.com/anjali-001">
+              here
+            </a>
+          </span>
         </p>
 
         <h3>What have I been up to?</h3>
         <p>
-          <span className="link-word"><a href="https://fellowship.mlh.io/" className="github-link">MLH Fellow'21</a></span>: Got selected for the
-          MLH Fellowship for the Spring'21 Batch and worked on awesome projects
-          with an amazing bunch of folks
+          <span className="link-word">
+            <a href="https://www.zfellows.com/" className="github-link">
+              Z Fellow
+            </a>
+          </span>
+          : Was a part of Z Fellows October'21 cohort for{" "}
+          <span className="link-word">
+            <a
+              href="https://www.producthunt.com/posts/deck-alpha"
+              className="github-link"
+            >
+              Deck
+            </a>
+          </span>
+          , an admin dashboard for resource sharing and access management.
         </p>
         <p>
           <span className="link-word">
-            <a href="https://dschit.com/" className="github-link">DSC-HIT, Core Team Member and Director of Outreach'19</a>
+            <a href="https://fellowship.mlh.io/" className="github-link">
+              MLH Fellow'21
+            </a>
+          </span>
+          : Got selected for the MLH Fellowship for the Spring'21 Batch and
+          worked on awesome projects with an amazing bunch of folks
+        </p>
+        <p>
+          <span className="link-word">
+            <a href="https://dschit.com/" className="github-link">
+              DSC-HIT, Core Team Member and Director of Outreach'19
+            </a>
           </span>
           : Will not be exaggerating if I say that a fair share of my initial
           tech journey is due to this community.
         </p>
-        <p>
-          These days I am building <span className="link-word"><a href="https://withdeck.com" className="github-link">Deck</a></span>, one
-          functionality at a time.
-        </p>
         <h3>Open source</h3>
-        <p>As someone who has constantly learnt from open source, it feels good to be able to contribute to projects that is used by someone. My recent contribution,</p>
         <p>
-          <span className="link-word"><a href="https://github.com/SimplQ/simplQ-frontend" className="github-link">SimpleQ</a></span>: It is a completely web based queue management solution that anyone can use to create instant virtual queues.
+          As someone who has constantly learnt from open source, it feels good
+          to be able to contribute to projects that is used by someone. My
+          recent contribution,
+        </p>
+        <p>
+          <span className="link-word">
+            <a
+              href="https://github.com/SimplQ/simplQ-frontend"
+              className="github-link"
+            >
+              SimpleQ
+            </a>
+          </span>
+          : It is a completely web based queue management solution that anyone
+          can use to create instant virtual queues.
         </p>
         {/* <br/> */}
         <p>
@@ -43,7 +79,15 @@ function Home() {
         </p>
         <p>
           Always up for a talk about web development(except arguments on
-          JavaScript Frameworks), art or mechanical keyboards. You can connect with me on <a href="https://twitter.com/_Anjali19_" className="github-link">Twitter</a> or drop a mail at <a href="mailto:hi@anjali.wtf" className="github-link">hi@anjali.wtf</a>{" "}
+          JavaScript Frameworks), art or mechanical keyboards. You can connect
+          with me on{" "}
+          <a href="https://twitter.com/_Anjali19_" className="github-link">
+            Twitter
+          </a>{" "}
+          or drop a mail at{" "}
+          <a href="mailto:hi@anjali.wtf" className="github-link">
+            hi@anjali.wtf
+          </a>{" "}
         </p>
       </div>
     </div>

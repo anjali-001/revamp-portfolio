b5f581a1cb8f83622971ea8d55a0c1232efb91d4
anjali-001 <anjali001.as@gmail.com>
2022-09-18 20:52:20 +0530

    removed unused imports

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,6 @@ import {Link} from 'react-router-dom'
 import './Navbar.css'
 import FeatherIcon from "feather-icons-react";
 import logodark from '../../assets/logodark.png'
-import menuIcon from '../../assets/menu-icon.svg'
 
 
 function Navbar() {

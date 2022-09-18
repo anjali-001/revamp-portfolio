3d99932d3411716c4d3804c099716c90ef483f9c
anjali-001 <anjali001.as@gmail.com>
2022-09-18 21:03:27 +0530

    updated resume

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
                 <Link to='/blog'>
                     <p>Blog</p>
                 </Link>
-                <a href='https://drive.google.com/file/d/1vuWJVjhZRHYZVRyGM1XiPQzT257WwaBQ/view?usp=sharing'>
+                <a target="_blank" rel='noreferrer' href='https://drive.google.com/file/d/1Erj36ybgX3M-iboqfzfoHmfDokZ1A7iN/view?usp=sharing'>
                     <p>Resume</p>
                 </a>
                 
